278ef5cf4bc8d776a5b946a193a4c9e0aad74bb3
Dyonovan <dyonovan@gmail.com>
2019-04-16 10:54:12 -0400

    Added COFHCore support

--- src/main/java/com/dyonovan/namedenchantments/NamedEnchantments.java
+++ src/main/java/com/dyonovan/namedenchantments/NamedEnchantments.java
@@ -14,7 +14,7 @@ public class NamedEnchantments
 {
     static final String MODID = "namedenchantments";
     static final String NAME = "Named Enchantments";
-    static final String VERSION = "1.2";
+    static final String VERSION = "1.3";
 
     static final Logger LOGGER = LogManager.getLogger(MODID);
 
